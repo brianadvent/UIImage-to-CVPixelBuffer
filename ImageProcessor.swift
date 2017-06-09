2dec673bce73d87b833984a5bd2e1d67b5e827dc
Brian Advent <kontakt@daniel-autenrieth.de>
2017-06-09 22:00:14 +0200

    Add files via upload

---
+++ ImageProcessor.swift
@@ -0,0 +1,40 @@
+//
+//  File.swift
+//  BasicCoreML
+//
+//  Created by Brian Advent on 09.06.17.
+//  Copyright © 2017 Brian Advent. All rights reserved.
+//
+
+import CoreVideo
+
+struct ImageProcessor {
+    static func pixelBuffer (forImage image:CGImage) -> CVPixelBuffer? {
+        
+        
+        let frameSize = CGSize(width: image.width, height: image.height)
+        
+        var pixelBuffer:CVPixelBuffer? = nil
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(frameSize.width), Int(frameSize.height), kCVPixelFormatType_32BGRA , nil, &pixelBuffer)
+        
+        if status != kCVReturnSuccess {
+            return nil
+            
+        }
+        
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags.init(rawValue: 0))
+        let data = CVPixelBufferGetBaseAddress(pixelBuffer!)
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let bitmapInfo = CGBitmapInfo(rawValue: CGBitmapInfo.byteOrder32Little.rawValue | CGImageAlphaInfo.premultipliedFirst.rawValue)
+        let context = CGContext(data: data, width: Int(frameSize.width), height: Int(frameSize.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: bitmapInfo.rawValue)
+        
+        
+        context?.draw(image, in: CGRect(x: 0, y: 0, width: image.width, height: image.height))
+        
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        
+        return pixelBuffer
+        
+    }
+    
+}
